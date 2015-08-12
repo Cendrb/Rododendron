8a47fe299a29041d3152ae5be516a8fcac07d8cf
Čeněk Drbal <cendrb@gmail.com>
2015-08-13 00:35:11 +0200

    f

--- src/main/java/com/cendrb/rododendron/item/ItemPinkIngot.java
+++ src/main/java/com/cendrb/rododendron/item/ItemPinkIngot.java
@@ -3,7 +3,10 @@ package com.cendrb.rododendron.item;
 import com.cendrb.rododendron.init.ModItems;
 import com.cendrb.rododendron.item.charger.ItemChargeableItem;
 import com.cendrb.rododendron.reference.Names;
+import com.cendrb.rododendron.utility.LogHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 /**
  * Created by cendr_000 on 6. 7. 2015.
@@ -25,4 +28,5 @@ public class ItemPinkIngot extends ItemChargeableItem {
     public ItemStack getChargingResult() {
         return new ItemStack(ModItems.energizedIngot);
     }
+    
 }
