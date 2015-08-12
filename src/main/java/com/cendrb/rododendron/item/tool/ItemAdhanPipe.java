0cd27a48388cf4328e472a3ecc91a8b34e35a106
Jan Svec <lphaozn@gmail.com>
2015-08-13 00:22:15 +0200

    Adhan Pipe

--- src/main/java/com/cendrb/rododendron/item/tool/ItemAdhanPipe.java
+++ src/main/java/com/cendrb/rododendron/item/tool/ItemAdhanPipe.java
@@ -2,6 +2,9 @@ package com.cendrb.rododendron.item.tool;
 
 import com.cendrb.rododendron.item.ItemGeneric;
 import com.cendrb.rododendron.reference.Names;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 /**
  * Created by Honza on 12. 8. 2015.
@@ -14,4 +17,10 @@ public class ItemAdhanPipe extends ItemGeneric {
         setUnlocalizedName(Names.Items.adhanPipe);
     }
 
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
+
+        world.playSoundEffect(player.posX, player.posY, player.posZ, "rododendron:adhan", 64, 1);
+        return itemStack;
+    }
 }
